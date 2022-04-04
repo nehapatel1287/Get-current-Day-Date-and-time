f7463867fd2a7689a9da6c1ffe218e959ae21242
Neha Patel <nehap.1287@gmail.com>
2022-04-04 22:34:13 +0530

    got first name from user and print

---
+++ get name from user and print/script.js
@@ -0,0 +1,10 @@
+function firstName (){
+    var person = prompt("Please enter your name");
+if (person != null) {
+  document.getElementById("Fname").value =
+  "Hello " + person + "! How are you today?";
+}
+}
+function hello(){
+    alert("hello");
+}
